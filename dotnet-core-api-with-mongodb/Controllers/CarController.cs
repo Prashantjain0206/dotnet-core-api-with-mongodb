d6ac0fd56a87be28d6be15fce43598d814de7f1f
Prashant Jain <deiprashant@gmail.com>
2021-01-05 00:59:16 +0530

    added space

--- dotnet-core-api-with-mongodb/Controllers/CarController.cs
+++ dotnet-core-api-with-mongodb/Controllers/CarController.cs
@@ -5,7 +5,7 @@ namespace dotnet_core_api_with_mongodb.Controllers
 {
     public class CarController : Controller
     {
-        // GET: CarController
+        // GET: Car Controller
         public ActionResult Index()
         {
             return View();
